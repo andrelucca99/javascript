c55bb60cfdfbcef4278891b1084fba13acab271f
André Lucas <66281231+andrelucca99@users.noreply.github.com>
2021-12-23 12:46:44 -0300

    Initial commit

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,40 @@
+//Repetição utilizando while()
+/*
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+*/
+
+//Repetição utilizando for()
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimpar(10)
+// Ou coloque - console.log(parimpar(1)) que funciona tbm.
